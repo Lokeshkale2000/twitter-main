00ea4c61e51992f734a42644ea8eb637d24bdda9
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2024-11-30 23:49:10 +0530

    next update

--- twitterclone/src/utils/constant.js
+++ twitterclone/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "https://twitter-main-orpin.vercel.app/?vercelToolbarCode=8ATm3MHgb-ENg_F/api/v1/user";
-export const TWEET_API_END_POINT = "https://twitter-main-orpin.vercel.app/?vercelToolbarCode=8ATm3MHgb-ENg_F/api/v1/tweet";
+export const USER_API_END_POINT ="https://twitter-main-seven.vercel.app/?vercelToolbarCode=ZWAdLjGUbOnK1ei/api/v1/user";
+export const TWEET_API_END_POINT ="https://twitter-main-seven.vercel.app/?vercelToolbarCode=ZWAdLjGUbOnK1ei/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
   let time = Date.parse(timestamp);
